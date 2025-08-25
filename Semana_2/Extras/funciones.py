675509cc5bf9acb59de459b3c0891488b404ef98
Mateo Rey <163074257+materey@users.noreply.github.com>
2025-08-25 09:39:25 -0300

    Subo archivo de practica semana 1 y clase funciones

---
+++ Semana_2/Extras/funciones.py
@@ -0,0 +1,8 @@
+def saludar(nombre):
+    print(f"Hola {nombre}")
+
+
+def promedio(a:int, b:int, c:int)-> float:
+    #resultado = (a + b + c) /3
+    #return resultado
+    return (a + b + c) /3
---
+++ Semana_2/Extras/main.py
@@ -0,0 +1,15 @@
+import funciones
+
+print("Bienvenidos al programa")
+
+nombre = input("Ingresar nombre: ")
+funciones.saludar(nombre)
+
+numero_1 = int(input("Numero 1: "))
+numero_2 = int(input("Numero 2: "))
+numero_3 = int(input("Numero 3: "))
+
+resultado_promedio = funciones.promedio(numero_1, numero_2, numero_3)
+
+#Al ser una funcion que devuelve el resultado en un dato especifico, se necesita de guardarlo en una variable
+
