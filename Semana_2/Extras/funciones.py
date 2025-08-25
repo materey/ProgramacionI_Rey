ccd2829d8e93c63e11275559a8307b524ce6e64d
Mateo Rey <163074257+materey@users.noreply.github.com>
2025-08-25 09:52:53 -0300

    Subo funcion operar

--- Semana_2/Extras/funciones.py
+++ Semana_2/Extras/funciones.py
@@ -6,3 +6,10 @@ def promedio(a:int, b:int, c:int)-> float:
     #resultado = (a + b + c) /3
     #return resultado
     return (a + b + c) /3
+
+
+def operar(a:int, b:int)->int:
+    suma = a + b
+    resta = a - b 
+    multiplicacion = a * b
+    return suma, resta, multiplicacion
--- Semana_2/Extras/main.py
+++ Semana_2/Extras/main.py
@@ -13,3 +13,7 @@ resultado_promedio = funciones.promedio(numero_1, numero_2, numero_3)
 
 #Al ser una funcion que devuelve el resultado en un dato especifico, se necesita de guardarlo en una variable
 
+
+
+resultado_suma, resultado_resta, resultado_multiplicacion = funciones.operar(numero_1, numero_2)
+#Se debe respetar el orden de las variables puestas en la funcion
